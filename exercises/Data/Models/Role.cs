aadbe8578e17427d4bd0208d169a6635af391faa
UnderNet <dimazimber@yandex.ru>
2022-08-11 18:46:13 +0300

    role

---
+++ exercises/Data/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace exercises.Data.Models
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+        [Required]
+        public string RoleName { get; set; }
+
+        public virtual int StudentID { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
